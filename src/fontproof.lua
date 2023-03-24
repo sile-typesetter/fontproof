0b871ee8fc52ae8d530e99253fabc904d10941ea
Caleb Maclennan <caleb@alerque.com>
2023-03-24 15:19:35 +0300

    feat: Work up script to run fontproof as a SILE wrapper

--- src/fontproof.lua
+++ src/fontproof.lua
@@ -1,6 +1,13 @@
-#!/usr/bin/env lua
+local bin = arg[0]
+local sile = "sile"
 
--- require "fontproof.main"
--- return fontproof
+local args = table.concat(arg, " ")
 
-print(66)
+local _, status, signal = os.execute(("%s -e 'print(%s)' -e 'os.exit(1)' %s"):format(sile, 44, args))
+
+if status == "exit" then
+	os.exit(signal)
+else
+	error(("Interupted with signal %s"):format(signal))
+	os.exit(1)
+end
