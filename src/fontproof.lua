527852cb1d7bd7b8628c6219f784b1ad7f079ccb
Caleb Maclennan <caleb@alerque.com>
2023-03-24 18:31:36 +0300

    refactor: Cleanup dev cruft so I can see what's going on

--- src/fontproof.lua
+++ src/fontproof.lua
@@ -1,19 +1,4 @@
-local D = require("pl.pretty").dump
-
 local cliargs = require("cliargs")
--- local lfs = require("lfs")
-local path = require("pl.path")
-local stringx = require("pl.stringx")
-
-local function script_path ()
-  local src = debug.getinfo(2, "S").source:sub(2)
-  D(src)
-  local base = src:match("(.*[/\\])")
-  return base
-end
-
-local bin = arg[0]
-local sile = "sile"
 
 cliargs:set_name("fontproof")
 cliargs:set_description([[
@@ -35,38 +20,15 @@ if not opts and parse_err then
    os.exit(1)
 end
 
-local template = opts.template and ("templates/%s.sil"):format(opts.template) or ""
--- if opts.template then
-   -- local cwd = lfs.currentdir()
-   -- local templatesdir = path.abspath(script_path() .. "/../templates")
-   -- D(templatesdir)
-   -- local foo = SILE.resolveFile(("fontproof/templates/%s.sil"):format(opts.template))
-   -- local template = 
-
-   -- local pp = stringx.split(package.path, "?")[1] .. "templates"
-   -- error("tt")
-   -- local templatefile = ("%s/%s.sil"):format(templatesdir, opts.template)
-   -- D(templatefile)
-   -- opts.template = path.exists(templatefile) and templatefile or error("Unknown template")
-   -- if not path.exists(opts.template) then
-   -- end
--- end
-
 local filename = opts.filename and ("-e _pfFilename='%s'"):format(opts.filename) or ""
 local size = opts.size and ("-e _pfSize='%s'"):format(opts.size) or ""
+local template = opts.template and ("templates/%s.sil"):format(opts.template) or ""
 local family = opts.family and ("-e _pfFamily='%s'"):format(opts.family) or ""
--- local template = opts.template and ("-e _fpTemplate='%s'"):format(opts.template) or ""
-local output = ("-o %s"):format(opts.ouptut or "fontproof.pdf")
+local output = ("-o %s"):format(opts.output or "fontproof.pdf")
 local args = opts.SILEARGS and opts.SILEARGS or ""
 
--- if opts.font then
---    D(opts.font)
---    local attrs = lfs.attributes(opts.font, "mode")
---    error("STOPME")
--- end
-
 local _, status, signal =
-   os.execute(table.concat({sile, filename, family, size, template, output, args}, " "))
+   os.execute(table.concat({"sile", filename, family, size, template, output, args}, " "))
 
 if status == "exit" then
    os.exit(signal)
